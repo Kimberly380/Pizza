b065c8a734353999d15e3ac96f4d15669ff99894
Kim Feinberg <kimberly380@gmail.com>
2016-03-22 11:22:50 -0700

    initial setup of files

--- js/app.js
+++ js/app.js
@@ -1,52 +1,9 @@
 
-var exports = {}; //dummy declaraion to let this file co-exist with test.js.
+//define min/max random function
 
+function minMax (min, max) {
+  return Math.floor(Math.random() * (max-min)) + min;
+}
 
-//problem 1 output
-var pProblemOneQ  = document.getElementById('Problem One Q');
-var pProblemOneA = document.getElementById('Problem One A');
-
-var num1 = 10;
-var num2 = 20;
-pProblemOneQ.textContent = "Problem 1: Write a function called sum() that takes in two numbers as arguments and then returns the sum of those arguments. Input: " + num1 + " and " + num2;
-// pProblemOneA.textContent = 'sumTwo returned ' + sumTwo(num1, num2);
-
-
-//problem 2 output
-var pProblemTwoQ  = document.getElementById('Problem Two Q');
-var pProblemTwoA = document.getElementById('Problem Two A');
-
-var num1 = 5;
-var num2 = 10;
-pProblemTwoQ.textContent  = "Problem 2: Write a function called multiply() that takes in two numbers as arguments and then returns the product of those arguments. Inputs: " + num1 + " and " + num2;
-// pProblemTwoA.textContent = 'sumTwo returned ' + multTwo(num1, num2);
-//
-//
-//problem 3 output
-var pProblemThreeQ  = document.getElementById('Problem Three Q');
-var pProblemThreeA = document.getElementById('Problem Three A');
-
-var num1 = 5;
-var num2 = 10;
-var num3 = 2;
-pProblemThreeQ.textContent  = "Problem 3: Write a function called sumAndMultiply() that takes in three numbers as arguments and then returns two values in an array: he sum of those numbers and the product of those numbers. Input: " + num1 + " and " + num2 + " and " + num3;
-// pProblemThreeA.textContent = 'sumTwo returned ' + sumAndMult(num1, num2, num3);
-
-
-//problem 4 output
-//REMEMBER: ANSWER SAYS NEED TO 'CONSOLE.TABLE() THE ARRAY FOR THIS ITEM AND NEXT.'
-var pProblemFourQ  = document.getElementById('Problem Four Q');
-var pProblemFourA = document.getElementById('Problem Four A');
-
-var array1 = [2,4,5];
-pProblemFourQ.textContent  = "Problem 4: Write a function called sumArray() that takes in an array of numbers as a single argument and then returns the sum of those numbers.  Input: " + array1;
-// pProblemFourA.textContent = 'sumArray returned ' + sumArray(array1);
-
-
-//problem 6 output
-var pProblemFiveQ  = document.getElementById('Problem Five Q');
-var pProblemFiveA = document.getElementById('Problem Five A');
-
-var array1 = [2,4,5];
-pProblemFiveQ.textContent  = "Problem 5: Write a function called multiplyArray() that takes in an array of numbers as a single argument and then returns the product of those numbers.  Input: " + array1;
-// pProblemFiveA.textContent = 'multiplyArray returned ' + multArray(array1);
+var randomNum = document.getElementById("minMax");
+randomNum.textContent = minMax(0,4);
